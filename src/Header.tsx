6adfc8ba6d4f3a87c52afdb1fc0820d510fc920d
Oleg <shevchenko.oleg.87@gmail.com>
2024-11-19 12:39:20 +0300

    patch wording in push.yml

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,10 +9,16 @@ const Header = () => {
                 <div className="message hidden"></div>
                 <div className="message-remove hidden"></div>
                 <div className="container">
-                    <h1 className="offer__title">Стеклянные елочные украшения фабрики "Ариель"</h1>
-                    <div className="offer__description">Здесь Вы можете оформить заявку на покупку наших изделий.</div>
+                    <h1 className="offer__title">
+                        Стеклянные елочные украшения фабрики "Ариель"
+                    </h1>
+                    <div className="offer__description">
+                        Здесь Вы можете оформить заявку на покупку наших изделий.
+                    </div>
                     <a className="offer__button" target='_blank' rel='noreferrer'
-                       href="https://f-ariel.ru/dokumenty/instrukciya_order.pdf">Как заказать?</a>
+                       href="https://f-ariel.ru/dokumenty/instrukciya_order.pdf">
+                        Как заказать?
+                    </a>
                 </div>
             </div>
         </>
